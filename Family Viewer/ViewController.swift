83efa169cdc9572dafe2fba8b622fde0cb395fa1
Ezekiel <ezekielelin@me.com>
2015-06-15 17:26:44 -0400

    "View" button works

--- Family Viewer/ViewController.swift
+++ Family Viewer/ViewController.swift
@@ -13,23 +13,36 @@ class ViewController: NSViewController {
     @IBOutlet weak var peopleCountLabel: NSTextField!
     @IBOutlet weak var personSelectPopup: NSPopUpButton!
     
+    ///Label for the name ("Name:")
+    @IBOutlet weak var nameLabel: NSTextField!
+    ///Label that shows the name
+    @IBOutlet weak var nameField: NSTextField!
+    ///Label for Parent A ("Parent A:")
+    @IBOutlet weak var parentALabel: NSTextField!
+    ///Label for Parent A's name
+    @IBOutlet weak var parentAField: NSTextField!
+    ///Label for Parent B ("Parent B:")
+    @IBOutlet weak var parentBLabel: NSTextField!
+    ///Label for Parent B's name
+    @IBOutlet weak var parentBField: NSTextField!
+    ///Add Parent A button
+    @IBOutlet weak var addParentA: NSButton!
+    ///Add Parent B button
+    @IBOutlet weak var addParentB: NSButton!
+    ///Remove Parent A
+    @IBOutlet weak var removeParentA: NSButton!
+    ///Remove Parent B
+    @IBOutlet weak var removeParentB: NSButton!
+    ///View Parent A
+    @IBOutlet weak var viewParentA: NSButton!
+    ///View Parent B
+    @IBOutlet weak var viewParentB: NSButton!
+    
     override func viewDidLoad() {
     
         NSNotificationCenter.defaultCenter().addObserver(self, selector: "treeDidUpdate", name: "com.ezekielelin.treeDidUpdate", object: nil)
         
     }
-    
-    var hasTree: Bool {
-        get {
-            if let _ = self.representedObject as? Tree {
-                return true
-            } else {
-                return false
-            }
-        }
-    }
-    
-    var popupArray = [Person]()
     
     var tree: Tree {
         set(newValue) {
@@ -45,26 +58,94 @@ class ViewController: NSViewController {
     func updateViewFromTree() {
         self.filenameLabel.stringValue = "Family Tree"
         self.peopleCountLabel.stringValue = self.tree.description
+        
         self.personSelectPopup.addItemsWithTitles(self.tree.indexOfPeople)
         
-//        print(self.tree.dictionary.writeToFile("/Users/ezekielelin/Desktop/test.xml",
+        selectNewPerson(self)
     }
     
-    func treeDidUpdate() {
-        updateViewFromTree()
+    func treeDidUpdate() { updateViewFromTree() }
+    
+    func currentPerson() -> Person? {
+        if self.personSelectPopup.indexOfSelectedItem == 0 {
+            return nil
+        }
+
+        return self.tree.people[self.personSelectPopup.indexOfSelectedItem - 1]
+
+    }
+    
+    func noPersonSelected() {
+        nameLabel.hidden = true
+        nameField.hidden = true
+        
+        parentALabel.hidden = true
+        parentAField.hidden = true
+        removeParentA.hidden = true
+        addParentA.hidden = true
+        viewParentA.hidden = true
+        
+        parentBLabel.hidden = true
+        parentBField.hidden = true
+        removeParentB.hidden = true
+        addParentB.hidden = true
+        viewParentB.hidden = true
+    }
+    
+    func selectPerson(person p: Person) {
+        personSelectPopup.selectItemWithTitle(p.description)
+    }
+    
+    @IBAction func viewParentA(sender: AnyObject) {
+        if let parentA = currentPerson()?.parentA {
+            selectPerson(person: parentA)
+            selectNewPerson(self)
+        }
+    }
+    
+    @IBAction func viewParentB(sender: AnyObject) {
+        if let parentB = currentPerson()?.parentB {
+            selectPerson(person: parentB)
+            selectNewPerson(self)
+        }
     }
     
     @IBAction func selectNewPerson(sender: AnyObject) {
-        if self.personSelectPopup.indexOfSelectedItem == 0 {
+        guard let person = currentPerson() else {
+            noPersonSelected()
             return
         }
-        let person = self.tree.people[self.personSelectPopup.indexOfSelectedItem - 1]
-        print(person.description)
-        print("\(person.parentA), \(person.parentB)")
-        print(person.children)
-        print(person.birth.date)
-        if !person.isAlive {
-            print(person.death.date)
+        
+        nameLabel.hidden = false
+        nameField.hidden = false
+        nameField.stringValue = person.description
+        
+        parentALabel.hidden = false
+        if let parentA = person.parentA {
+            addParentA.hidden = true
+            removeParentA.hidden = false
+            viewParentA.hidden = false
+            parentAField.hidden = false
+            parentAField.stringValue = parentA.description
+        } else {
+            addParentA.hidden = false
+            removeParentA.hidden = true
+            viewParentA.hidden = true
+            parentAField.hidden = true
+        }
+        
+        parentBLabel.hidden = false
+        if let parentB = person.parentB {
+            addParentB.hidden = true
+            removeParentB.hidden = false
+            viewParentB.hidden = false
+            parentBField.hidden = false
+            parentBField.stringValue = parentB.description
+        } else {
+            addParentB.hidden = false
+            removeParentB.hidden = true
+            viewParentB.hidden = true
+            parentBField.hidden = true
         }
     }
 }
